33481f475078e3deae82fb30553ac235aff05597
Pushpa Raj Badu <pushparaj.badu@gmail.com>
2021-01-02 08:37:21 +0545

    update production domain

--- src/constants/index.js
+++ src/constants/index.js
@@ -1,11 +1,11 @@
 let API_URL;
 let WS_URL;
 if (process.env.NODE_ENV === "production") {
-  API_URL = "https://qbauth.herokuapp.com";
-  WS_URL = "ws://qbauth.herokuapp.com/cable";
+  API_URL = "http://qboauth.prod";
+  WS_URL = "wss://qboauth.prod/cable";
 } else {
   API_URL = "http://localhost:3000";
-  WS_URL = "ws://localhost:3000/cable";
+  WS_URL = "wss://localhost:3000/cable";
 }
 
 export { API_URL, WS_URL };
