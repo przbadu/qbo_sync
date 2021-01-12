23370d80c892d67ab57621730d669bff9038e8f9
Pushpa Raj Badu <pushparaj.badu@gmail.com>
2021-01-12 21:28:24 +0545

    update production domain

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -1,8 +1,8 @@
 let API_URL;
 let WS_URL;
 if (process.env.NODE_ENV === "production") {
-  API_URL = "http://qboauth.prod";
-  WS_URL = "wss://qboauth.prod/cable";
+  API_URL = "https://qboapi.tk";
+  WS_URL = "wss://qboapi.tk/cable";
 } else {
   API_URL = "http://localhost:3000";
   WS_URL = "ws://localhost:3000/cable";
