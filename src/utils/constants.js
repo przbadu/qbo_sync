644a7ab395a9dc1bf6eec2d19e1ba96f97c2cf01
Pushpa Raj Badu <pushparaj.badu@gmail.com>
2021-01-15 11:26:42 +0545

    fixing ws url

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -2,7 +2,7 @@ let API_URL;
 let WS_URL;
 if (process.env.NODE_ENV === "production") {
   API_URL = "https://qboapi.tk";
-  WS_URL = "wss://qboapi.tk/cable";
+  WS_URL = "ws://qboapi.tk/cable";
 } else {
   API_URL = "http://localhost:3000";
   WS_URL = "ws://localhost:3000/cable";
